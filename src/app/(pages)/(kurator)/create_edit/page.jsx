72578a9dccac218da3ca93217f3afe3b4b1b7d9c
Mai El-Zein <mai_ez@outlook.dk>
2025-06-03 15:20:40 +0200

    Få react HookForm til at virke

--- src/app/(pages)/(kurator)/create_edit/page.jsx
+++ src/app/(pages)/(kurator)/create_edit/page.jsx
@@ -1,5 +1,10 @@
 import KuratorForm from "@/components/kurator_create_edit/KuratorForm";
 
-export default async function CreateEditEventPage() {
-  return KuratorForm;
+export default function CreateEditEventPage() {
+  return (
+    <main>
+      <h1>Create Edit</h1>
+      <KuratorForm></KuratorForm>
+    </main>
+  );
 }
--- src/components/kurator_create_edit/KuratorForm.jsx
+++ src/components/kurator_create_edit/KuratorForm.jsx
@@ -1,9 +1,46 @@
 "use client";
 import { useForm, SubmitHandler } from "react-hook-form";
+import CustomButton from "../global/CustomButton";
 const KuratorForm = () => {
-  <form>
-    <input></input>
-  </form>;
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        console.log(data);
+      })}
+      className="flex flex-col gap-(--space-2rem)"
+    >
+      <input
+        className="border-2 border-amber-700"
+        defaultValue={"Titel"}
+        {...register("Titel")}
+      ></input>
+      <input
+        className="border-2 border-amber-700"
+        defaultValue={"Dato"}
+        {...register("Dato")}
+      ></input>
+      <input
+        className="border-2 border-amber-700"
+        defaultValue={"Lokation"}
+        {...register("Lokation")}
+      ></input>
+      <input
+        className="border-2 border-amber-700"
+        defaultValue={"Beskrivelse"}
+        {...register("Beskrivelse")}
+      ></input>
+      <section
+        className="border-2 border-amber-700"
+        defaultValue={"Billeder"}
+        {...register("Billeder")}
+      >
+        <Image></Image>
+      </section>
+      <CustomButton type="Submit" text="Submit"></CustomButton>
+    </form>
+  );
 };
 
 export default KuratorForm;
