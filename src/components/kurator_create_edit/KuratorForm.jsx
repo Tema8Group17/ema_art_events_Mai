d3e508480f62420922666068e5e745b400ed2223
Mai El-Zein <mai_ez@outlook.dk>
2025-06-05 13:08:47 +0200

    alt virker

--- src/components/kurator_create_edit/KuratorForm.jsx
+++ src/components/kurator_create_edit/KuratorForm.jsx
@@ -14,19 +14,13 @@ import { useSearchParams } from "next/navigation";
 const KuratorForm = ({ images, locations, prevData }) => {
   // PATCH
 
-  //
-  // console.log(
-  //   "KuratorForm: ",
-  //   "prevData",
-  //   prevData,
-  //   "prevData.artworkIds",
-  //   prevData.artworkIds
-  // );
   const { register, handleSubmit } = useForm({
     defaultValues: prevData || {},
   });
   // const [isSelected, setIsSelected] = useState();
-  const [selectedImages, setSelectedImages] = useState([]);
+  const [selectedImages, setSelectedImages] = useState(
+    prevData?.artworkIds || []
+  );
 
   const onSubmit = async (data) => {
     const indhold = {
@@ -39,22 +33,8 @@ const KuratorForm = ({ images, locations, prevData }) => {
     if (prevData && prevData.id) {
       await updateEvent(prevData.id, indhold);
     } else {
-      const indhold = {
-        title: data.title,
-        date: data.date,
-        locationId: data.locationId,
-        description: data.description,
-        artworkIds: selectedImages,
-      };
-
-      console.log(
-        "onSubmit function: ",
-        "indhold",
-        indhold,
-        "selectedImages",
-        selectedImages
-      );
-      const opret = await createEvent(indhold);
+      console.log("indhold", indhold);
+      await createEvent(indhold);
     }
   };
 
@@ -98,42 +78,15 @@ const KuratorForm = ({ images, locations, prevData }) => {
             return (
               <Image
                 onClick={() => {
-                  if (prevData) {
-                    // ----------------- id prevData ------------------------- //
-                    const newSelection = selectedImages.includes(
-                      img.object_number
-                    )
-                      ? selectedImages.filter(
-                          (item) => item !== img.object_number
-                        )
-                      : selectedImages.concat(img.object_number);
-
-                    prevData.artworkIds.map((previtems) =>
-                      previtems.concat(newSelection)
-                    );
-
-                    setSelectedImages(newSelection);
-                    console.log(
-                      "onClick: if prevData: ",
-                      "slectedImages",
-                      selectedImages,
-                      "prevData.artworkIds",
-                      prevData.artworkIds,
-                      "newSelection: ",
-                      newSelection
-                    );
-                  }
-                  // --------------------------- if not prevData -----------------------//
-                  else {
-                    setSelectedImages(
-                      selectedImages.includes(img.object_number)
-                        ? selectedImages.filter(
-                            (item) => item !== img.object_number
-                          )
-                        : selectedImages.concat(img.object_number)
-                    );
-                    console.log("onClick: selectedImages", selectedImages);
-                  }
+                  const newSelection = selectedImages.includes(
+                    img.object_number
+                  )
+                    ? selectedImages.filter(
+                        (item) => item !== img.object_number
+                      )
+                    : selectedImages.concat(img.object_number);
+                  console.log("onClick: newSelection", newSelection);
+                  setSelectedImages(newSelection);
                 }}
                 key={img.object_number}
                 src={img.image_thumbnail || img.image_native || Placeholder}
@@ -141,7 +94,6 @@ const KuratorForm = ({ images, locations, prevData }) => {
                 height={img.image_height || 400}
                 alt={img.title || "SMK billede"}
                 className={`object-cover w-full h-full col-span-1 row-span-1 ${
-                  // hvis prevData har artworkIds der matcher object_number så skal border vises medmindre selectedImage er mere end 0
                   selectedImages.length === 0
                     ? prevData?.artworkIds?.includes(img.object_number)
                       ? "border-4 border-green-500 order-first"
