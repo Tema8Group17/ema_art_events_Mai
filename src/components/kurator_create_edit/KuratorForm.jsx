fedf474e99020270336e4240beb8817af756acb8
ElMaez <mai_ez@outlook.dk>
2025-06-09 20:19:20 +0200

    shadcn opsætning skal skrives væk fra typescript

--- src/components/kurator_create_edit/KuratorForm.jsx
+++ src/components/kurator_create_edit/KuratorForm.jsx
@@ -11,25 +11,23 @@ import Filter from "../global/filter/Filter";
 import { filterData } from "../global/filter/actions";
 import GalleryImage from "./GalleryImage";
 
+// ------------------------------- ShadCn ------------------------------------- //
+import * as React from "react";
+import { ChevronDownIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 // ----------------------------- KuratorForm ---------------------------------- //
 const KuratorForm = ({ images, locations, prevData, categories, events }) => {
-  // Filter
-  const [state, action, isPending] = useActionState(filterData, {
-    active: [],
-    data: [],
-  });
-
-  function handleFilter(value, category) {
-    const replaceFilter = state?.active?.filter(
-      (item) => !item.includes(category)
-    );
-    const data =
-      value === "all"
-        ? replaceFilter
-        : [...replaceFilter, `[${category}:${value}]`];
-
-    startTransition(action.bind(state, data));
-  }
+  // Shadcn Calender
+  const [open, setOpen] = React.useState(false);
+  const [date, setDate] = (React.useState < Date) | (undefined > undefined);
 
   // PATCH
 
@@ -71,6 +69,37 @@ const KuratorForm = ({ images, locations, prevData, categories, events }) => {
         defaultValue={"Title"}
         {...register("title")}
       ></input>
+      {/* Shadcn start */}
+      <div className="flex flex-col gap-3">
+        <Label htmlFor="date" className="px-1">
+          Date of birth
+        </Label>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              id="date"
+              className="w-48 justify-between font-normal"
+            >
+              {date ? date.toLocaleDateString() : "Select date"}
+              <ChevronDownIcon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={date}
+              captionLayout="dropdown"
+              onSelect={(date) => {
+                setDate(date);
+                setOpen(false);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+      {/* Shadcn end */}
+
       <input
         className="border-2 border-amber-700"
         defaultValue={"Dato"}
